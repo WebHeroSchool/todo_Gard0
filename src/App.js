5b1e1e96efd9be310c5c9d767b3cf0d956cb0730
Mike <michael.fomin.89@gmail.com>
2021-03-03 23:36:14 +0300

    JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+const flag = true;
+const flag2 = false;
+const count = 22;
+
 function App() {
   return (
     <div className="App">
@@ -9,9 +13,21 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
+        <p style={{ color: 'blue', fontSize: 46 - 12 }}>
           Hello Ауад Майз!
         </p>
+        <p>
+          {count}
+        </p>
+        <p>
+          {flag && 'Flag is true'}
+        </p>
+        <p style={{ color: 'red' }}> undefined {undefined} , null  {null}, true  {true} и false {false}
+        </p>
+        <p>
+          {flag2 ? 'Flag is true' : 'Flag is false'}
+        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
