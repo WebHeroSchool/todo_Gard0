998723b29e8736bc2726282a2c33757b2a12f135
Mike <michael.fomin.89@gmail.com>
2021-03-20 14:16:47 +0300

    Structure

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList';
+import Item from '../Item/Item';
+
+const App = () => (
+  <div>
+    <h1>Важные дела</h1>
+    <ItemList />
+  </div>
+);
+
+export default App;
---
+++ src/components/Item/Item.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Item = () => (
+    <span>Очень важное дело</span>);
+
+export default Item;
