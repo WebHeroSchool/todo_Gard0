b815b73a75e119de17c14c1a72de5de749743d32
Mike <michael.fomin.89@gmail.com>
2021-05-06 16:16:33 +0300

    small fix 06.05.21

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -4,10 +4,9 @@ import TextField from '@material-ui/core/TextField';
 const InputItem = () => (<div>
     <TextField
         id="standard-dense"
-        label="Добавить задание"
+        label="Добавить дело"
         margin="dense"
     />
 </div>);
-
 
 export default InputItem;
