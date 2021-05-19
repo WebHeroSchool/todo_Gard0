04fe22e76fd1181dbfca0b86758c32252e3cc616
Mike <michael.fomin.89@gmail.com>
2021-05-19 19:50:17 +0300

    code refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,40 +5,40 @@ import ItemList from '../ItemList/ItemList.jsx';
 import Footer from '../Footer/Footer.jsx';
 
 
+class App extends React.Component {
+  render() {
+    const toDo = [
+      {
+        value: 'Написать приложение',
+        isDone: true
+      },
+      {
+        value: 'прописать props',
+        isDone: false
+      },
+      {
+        value: 'сделать все дела',
+        isDone: false
+      }
+    ]
 
-const App = () => {
-
-  const toDo = [
-    {
-      value: 'Написать приложение',
-      isDone: true
-    },
-    {
-      value: 'прописать props',
-      isDone: false
-    },
-    {
-      value: 'сделать все дела',
-      isDone: false
-    }
-  ]
-
-  let toDoFilter = toDo.filter(toDo => toDo.isDone === false);
-  let toDoList = toDoFilter.length;
-
+    let toDoFilter = toDo.filter(toDo => toDo.isDone === false);
+    let toDoList = toDoFilter.length;
 
 
-  return (
 
-    <div className={styles.wrap}>
-      <h2 className={styles.title}>Важные дела</h2>
-      <InputItem />
-      <div>
-        <ItemList item={toDo} />
-        <Footer count={toDoList} />
-      </div>
-    </div >
-  );
+    return (
+
+      <div className={styles.wrap}>
+        <h2 className={styles.title}>Важные дела</h2>
+        <InputItem />
+        <div>
+          <ItemList item={toDo} />
+          <Footer count={toDoList} />
+        </div>
+      </div >
+    );
+  };
 };
 
 export default App;
