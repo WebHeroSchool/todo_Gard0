d5cee6e5434f40399d62044ca61a23b0a94fb217
Persovt <mza200339@gmail.com>
2021-06-04 19:57:10 +0300

    razbor

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -31,16 +31,20 @@ class App extends React.Component {
 
 
   render() {
-    let toDoFilter = this.state.toDo.filter(toDo => toDo.isDone === false);
-
+  
+    // return (
+    //   <div className="">
+    //    <ItemList items={this.state.toDo} onClickDone={this.onClickDone} />
+    //   </div>
+    // )
     return (
       
       <div className={styles.wrap}>
         <h2 className={styles.title}>Важные дела</h2>
         <InputItem />
         <div>
-          <ItemList item={this.state.toDo} onClickDone={this.onClickDone} />
-          <Footer count={toDoFilter.length} />
+          <ItemList items={this.state.toDo} onClickDone={this.onClickDone} />
+          <Footer count={this.state.toDo.filter(toDo => !toDo.isDone ).length} />
         </div>
       </div >
     );
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,28 +1,22 @@
-import React from 'react';
-import ListItem from '@material-ui/core/ListItem'
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
-import Checkbox from '@material-ui/core/Checkbox';
-import DeleteIcon from '@material-ui/core/DeleteIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import IconButton from '@material-ui/core/ListItemSecondaryAction';
-import { withStyles } from '@material-ui/core';
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import Checkbox from "@material-ui/core/Checkbox";
+import { Delete } from "@material-ui/icons";
+import { ListItemText } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
+import { withStyles } from "@material-ui/core";
 
+const Item = ({ value, isDone, onClickDone }) => (
+  <ListItem>
+    <Checkbox onClick={() => onClickDone(isDone)} />
+    <p>{value}</p>
+    <ListItemSecondaryAction>
+      <IconButton aria-label="New comment">
+        <Delete />
+      </IconButton>
+    </ListItemSecondaryAction>
+  </ListItem>
+);
 
-const Item = ({ value, isDone, classes, onClickDone }) => (
-    <ListItem fullWidth>
-        <Checkbox
-            checked={isDone}
-            tabIndex={-1}
-            onClick={() => onClickDone(isDone)}
-        />
-        <ListItemText primary={value}
-            classes={{ root: isDone && classes.isDone }}
-        />
-        <ListItemSecondaryAction>
-            <IconButton aria-label="New comment">
-                <DeleteIcon />
-            </IconButton>
-        </ListItemSecondaryAction>
-    </ListItem>);
-
-export default withStyles (Item);
+export default Item
