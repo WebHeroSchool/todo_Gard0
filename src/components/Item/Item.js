d5bf4d7e2d80d94c77c2b158bbc4df339f54d18d
Mike <michael.fomin.89@gmail.com>
2021-06-19 12:43:56 +0300

    setStateAdd

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,12 +4,23 @@ import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import Checkbox from "@material-ui/core/Checkbox";
 import { Delete } from "@material-ui/icons";
 import { IconButton } from "@material-ui/core";
+import styles from './Item.module.css';
+import classnames from 'classnames';
 
 
-const Item = ({ value, isDone, onClickDone }) => (
+const Item = ({ onClickDone, id, isDone, value }) => (
   <ListItem>
-    <Checkbox onClick={() => onClickDone(isDone)} />
-    <p>{value}</p>
+    <Checkbox
+      onClick={() => onClickDone(id)} />
+    <p
+      className={classnames({
+        [styles.inProgress]: false,
+        [styles.done]: isDone
+      })}
+    >
+      {value}
+    </p>
+
     <ListItemSecondaryAction>
       <IconButton aria-label="New comment">
         <Delete />
@@ -18,4 +29,4 @@ const Item = ({ value, isDone, onClickDone }) => (
   </ListItem>
 );
 
-export default Item
+export default Item;
